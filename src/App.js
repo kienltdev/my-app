57681d8594e4102b21ec2fa59d8688ada519efb5
momonga <kienlt.dev@gmail.com>
2024-04-02 18:04:39 +0700

    Deploying my react-appv4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CI/CD với github action, tự động deploy qua github pages
         </p>
         <a
           className="App-link"
